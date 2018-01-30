0df77a97f73b64897f686217222649d043d58e32
Heather Gallop <doyouseeitmyway@gmail.com>
2018-01-30 16:41:21 -0500

    Refine logic for innings. Comment java file.

--- app/src/main/java/com/example/android/scorekeeperbaseball/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeperbaseball/MainActivity.java
@@ -19,31 +19,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /** Displays home team score */
     public void displayForHome(int score) {
         TextView scoreView = (TextView) findViewById(R.id.runsHome);
         scoreView.setText(String.valueOf(score));
     }
 
+    /** Displays visiting team scoew */
     public void displayForVisitor(int score) {
         TextView scoreView = (TextView) findViewById(R.id.runsVisitor);
         scoreView.setText(String.valueOf(score));
     }
 
+    /** Displays home team outs */
     public void displayForHomeOuts(int score) {
         TextView scoreView = (TextView) findViewById(R.id.outsHome);
         scoreView.setText(String.valueOf(score));
     }
 
+    /** Displays visiting team outs */
     public void displayForVisitorOuts(int score) {
         TextView scoreView = (TextView) findViewById(R.id.outsVisitor);
         scoreView.setText(String.valueOf(score));
     }
 
+    /** Displays inning */
     public void displayInning(int inning) {
         TextView scoreView = (TextView) findViewById(R.id.inning);
         scoreView.setText(String.valueOf(inning));
     }
 
+    /** Resets all values to default */
     public void resetAll(View view) {
         outHome = 0;
         outVisitor = 0;
@@ -57,11 +63,13 @@ public class MainActivity extends AppCompatActivity {
         displayInning(inning);
     }
 
+    /** Calculates and displays home team run on button click */
     public void runScoreHome(View view) {
         runHome += 1;
         displayForHome(runHome);
     }
 
+    /** Calculates and displays visiting team run on button click */
     public void runScoreVisitor(View view) {
         runVisitor += 1;
         displayForVisitor(runVisitor);
@@ -77,41 +85,41 @@ public class MainActivity extends AppCompatActivity {
      * If inning is greater than 9
      * display Extra innings
      */
+    /** Calculates and displays home team outs. Control and display inning */
     public void outsHome(View view) {
+        if (outHome < 3) {
+            outHome += 1;
+        }
+        displayForHomeOuts(outHome);
         if (outHome == 3 && outVisitor == 3) {
+            outHome = 0;
+            outVisitor = 0;
+            displayForHomeOuts(outHome);
+            displayForVisitorOuts(outVisitor);
             if (inning < 9) {
                 inning += 1;
-                outHome = 0;
-                outVisitor = 0;
-                displayForHomeOuts(outHome);
-                displayForVisitorOuts(outVisitor);
-            } else {
-                displayInning(inning);
             }
             displayInning(inning);
-        } else {
-            outHome += 1;
-            displayForHomeOuts(outHome);
         }
 
 
     }
 
+    /** Calculates and displays visiting team outs. Control and display inning */
     public void outsVisitor(View view) {
+        if (outVisitor < 3) {
+            outVisitor += 1;
+        }
+        displayForVisitorOuts(outVisitor);
         if (outHome == 3 && outVisitor == 3) {
+            outHome = 0;
+            outVisitor = 0;
+            displayForHomeOuts(outHome);
+            displayForVisitorOuts(outVisitor);
             if (inning < 9) {
                 inning += 1;
-                outHome = 0;
-                outVisitor = 0;
-                displayForHomeOuts(outHome);
-                displayForVisitorOuts(outVisitor);
-            } else {
-                displayInning(inning);
             }
             displayInning(inning);
-        } else {
-            outVisitor += 1;
-            displayForVisitorOuts(outVisitor);
         }
     }
 
