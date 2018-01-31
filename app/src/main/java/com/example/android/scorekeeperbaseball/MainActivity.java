5f0da80a58265c6408ea30d5b9090c48dcca196a
Heather Gallop <doyouseeitmyway@gmail.com>
2018-01-31 08:38:17 -0500

    Reformat and rearrange code. Update comments in java.

--- app/src/main/java/com/example/android/scorekeeperbaseball/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeperbaseball/MainActivity.java
@@ -19,37 +19,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /** Displays home team score */
+    /**
+     * Displays home team score
+     */
     public void displayForHome(int score) {
         TextView scoreView = (TextView) findViewById(R.id.runsHome);
         scoreView.setText(String.valueOf(score));
     }
 
-    /** Displays visiting team scoew */
+    /**
+     * Displays visiting team scoew
+     */
     public void displayForVisitor(int score) {
         TextView scoreView = (TextView) findViewById(R.id.runsVisitor);
         scoreView.setText(String.valueOf(score));
     }
 
-    /** Displays home team outs */
+    /**
+     * Displays home team outs
+     */
     public void displayForHomeOuts(int score) {
         TextView scoreView = (TextView) findViewById(R.id.outsHome);
         scoreView.setText(String.valueOf(score));
     }
 
-    /** Displays visiting team outs */
+    /**
+     * Displays visiting team outs
+     */
     public void displayForVisitorOuts(int score) {
         TextView scoreView = (TextView) findViewById(R.id.outsVisitor);
         scoreView.setText(String.valueOf(score));
     }
 
-    /** Displays inning */
+    /**
+     * Displays inning
+     */
     public void displayInning(int inning) {
         TextView scoreView = (TextView) findViewById(R.id.inning);
         scoreView.setText(String.valueOf(inning));
     }
 
-    /** Resets all values to default */
+    /**
+     * Resets all values to default
+     */
     public void resetAll(View view) {
         outHome = 0;
         outVisitor = 0;
@@ -63,31 +75,41 @@ public class MainActivity extends AppCompatActivity {
         displayInning(inning);
     }
 
-    /** Calculates and displays home team run on button click */
+    /**
+     * Calculates and displays home team run on button click
+     */
     public void runScoreHome(View view) {
         runHome += 1;
         displayForHome(runHome);
     }
 
-    /** Calculates and displays visiting team run on button click */
+    /**
+     * Calculates and displays visiting team run on button click
+     */
     public void runScoreVisitor(View view) {
         runVisitor += 1;
         displayForVisitor(runVisitor);
     }
 
-    /** Calculates and displays visiting team run on button click */
+    /**
+     * Calculates and displays home team grandslam on button click
+     */
     public void grandSlamHome(View view) {
         runHome += 4;
         displayForHome(runHome);
     }
 
-    /** Calculates and displays visiting team run on button click */
-    public void  grandSlamVisitor(View view) {
+    /**
+     * Calculates and displays visiting team grandslam on button click
+     */
+    public void grandSlamVisitor(View view) {
         runVisitor += 4;
         displayForVisitor(runVisitor);
     }
 
-    /** Calculates and displays home team outs. Control and display inning */
+    /**
+     * Calculates and displays home team outs. Control and display inning
+     */
     public void outsHome(View view) {
         if (outHome < 3) {
             outHome += 1;
@@ -112,7 +134,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    /** Calculates and displays visiting team outs. Control and display inning */
+    /**
+     * Calculates and displays visiting team outs. Control and display inning
+     */
     public void outsVisitor(View view) {
         if (outVisitor < 3) {
             outVisitor += 1;
